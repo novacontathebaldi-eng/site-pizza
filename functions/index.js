98e9fae94b9c29aae691f88aaf63b0f4c21d4a36
othebaldi <compras.thebaldi@gmail.com>
2025-10-15 14:38:41 -0300

    e agora ok?

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-const {onCall, onRequest} = require("firebase-functions/v2/h");
+const {onCall, onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 const {MercadoPagoConfig, Payment, PaymentRefund} = require("mercadopago");
