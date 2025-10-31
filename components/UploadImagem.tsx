7e971a9cb7f82f1ffc7f15ded0297133b5811f10
othebaldi <compras.thebaldi@gmail.com>
2025-10-31 13:14:51 -0300

    MANDEI AQUI DE NOVO

--- components/UploadImagem.tsx
+++ components/UploadImagem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, ChangeEvent } from 'react';
-import { uploadImagem } from '../src/utils/uploadImagem.js';
+import { uploadImagem } from '@/src/utils/uploadImagem.js';
 
 export function UploadImagem() {
     const [status, setStatus] = useState('');
