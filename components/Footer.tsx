00d23b3eb1df947264de6b1dc7b899a889b0458a
othebaldi <compras.thebaldi@gmail.com>
2025-11-06 06:58:55 -0300

    Endereço VENDA

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -129,8 +129,8 @@ export const Footer: React.FC<FooterProps> = ({ settings, onOpenChatbot, onOpenP
                     <div>
                         <h4 className="font-bold text-lg mb-4">Contato</h4>
                         <ul className="space-y-2 text-brand-green-300">
-                            <li><i className="fas fa-map-marker-alt mr-2 text-accent"></i>Porfilio Furtado, 178 - Centro</li>
-                            <li>Santa Leopoldina, ES</li>
+                            <li><i className="fas fa-map-marker-alt mr-2 text-accent"></i>AV. Vitória, 221</li>
+                            <li>Vitória, ES</li>
                             <li><i className="fas fa-phone mr-2 text-accent"></i>(27) 99667-0426</li>
                         </ul>
                     </div>
