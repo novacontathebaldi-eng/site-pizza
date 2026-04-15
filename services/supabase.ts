171deff1abfc85ff07c2cbd7e5df4bd956ab1723
othebaldi <compras.thebaldi@gmail.com>
2026-04-15 16:14:00 -0300

    att

--- services/supabase.ts
+++ services/supabase.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://vzqaabulgvdygjzcouaz.supabase.co"
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_R6gg1EaTkuAOYIl0lmENWg_R0Qt372w"
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: localStorage
+  }
+})
